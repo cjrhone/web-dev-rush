67103385492ad7bb900f4692ce1080729e0a1dc1
Alex Noble <AWGR-Alex@comcast.net>
2018-04-09 11:34:57 -0700

    add function to test user input on enter against prompts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
+var nextStep=0;
+var bug=0;
+var testPrompt="<html>";
 
 //add listener to get textbox input when a user presses enter
 function runScript(e){
   if (e.keyCode==13){
     var userInput=$('#inputBox').val();
     console.log(userInput);
+    $('#inputBox').val("");
+    testUserInput(userInput); //test input on enter press
   } else{}
+
+}
+
+//test if input matches prompt
+function testUserInput(userInput){
+  if (userInput===testPrompt){
+    nextStep++;
+  } else{
+    bug++;
+  }
 }
 
 
