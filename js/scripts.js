6cccc31004db95d4fcacc00c460998d752bd4659
cj <charlesrhonejr@gmail.com>
2018-04-09 12:22:34 -0700

    Add prompt array

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,9 @@ function testUserInput(userInput){
 
 
 $(document).ready(function() {
-  $("#terminal-form").submit(function(event) {
 
-    alert("you submitted inputted");
-
-  })
 })
+
+var prompt =
+
+["<html>","</html>","<head>","</head>","<title>","</title>","Animal Shelter","<body>","</body>","<h1>","</h1>","Animal Shelter","<div class='column'>","</div>","<img src='img/dog1.jpg'>","<h2>","</h2>","Ben the Dog","<p>","</p>","Happy even though nobody loves him.","<div class='column'>","<img src='img/dog2.jpg'>","<h2>","</h2>","Loretta the Dog","<p>","</p>","Always looks sad.","<div class='column'>","</div>","<img src='img/dog3.jpg'>","<h2>","</h2>","Billy the Dog","<p>","</p>","Loves the beach!","<br>","<div class='column'>","</div>","<img src='img/cat1.jpg'>","<h2>","</h2>","Greg the Cat","<p>","</p>","<div class='column'>","</div>""<img src='img/cat2.jpg'>","<h2>","</h2>","Tanya the Cat","<p>","</p>","Eats a lot, including her last owner.","<div class='column'>","</div>","<img src='img/cat3.jpg'>","<h2>","</h2>","<p>","</p>","Harry is a wild man!"];
