77252df461c53d65fb739d9e4e551227f804f01d
Alex Noble <AWGR-Alex@comcast.net>
2018-04-09 11:14:24 -0700

    add textbox that gets textvalue when a user presses enter

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+
+//add listener to get textbox input when a user presses enter
+function runScript(e){
+  if (e.keyCode==13){
+    var userInput=$('#inputBox').val();
+    console.log(userInput);
+  } else{}
+}
+
+
 $(document).ready(function() {
   $("#terminal-form").submit(function(event) {
 
