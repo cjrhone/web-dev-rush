493a2c7585497e63ecab17807c749eb7aacf578b
Jim Palowski <palowskijim@gmail.com>
2018-04-09 11:54:43 -0700

    Add About html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  $("#terminal-form").submit(function(event) {
-
-    alert("you submitted inputted");
-
-  })
+$("#startGame").submit(function(event){
+  event.preventDefault();
+  $(".game").show();
+  $(".closeGame").hide();
+})
 })
